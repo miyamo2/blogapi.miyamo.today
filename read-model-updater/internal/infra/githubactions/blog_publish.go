0db7535795fe8e528fae6227b1aa0cb927edf2bb
miyamo2 <miyamo2@outlook.com>
2024-12-09 02:01:54 +0900

    feat(read-model-updater): impl publisher

--- read-model-updater/internal/infra/githubactions/blog_publish.go
+++ read-model-updater/internal/infra/githubactions/blog_publish.go
@@ -1,8 +1,12 @@
 package githubactions
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
 )
+
+const payload = `{"event_type":"sync-read-model"}}`
 
 type Client interface {
 	Do(req *http.Request) (*http.Response, error)
@@ -15,14 +19,16 @@ type BlogPublisher struct {
 }
 
 func (b *BlogPublisher) Publish() error {
-	//req, err := http.NewRequest(http.MethodPost, b.endpoint, nil)
-	//if err != nil {
-	//	return err
-	//}
-	//req.Header.Set("Authorization", b.token)
-	//if _, err = b.client.Do(req); err != nil {
-	//	return err
-	//}
+	req, err := http.NewRequest(http.MethodPost, b.endpoint, bytes.NewBuffer([]byte(payload)))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", b.token))
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+	if _, err = b.client.Do(req); err != nil {
+		return err
+	}
 	return nil
 }
 
