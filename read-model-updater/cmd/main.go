3cf4235e19b2243f290e193462950df4b36580d5
miyamo2 <miyamo2@outlook.com>
2024-12-09 04:57:16 +0900

    debug

--- read-model-updater/cmd/main.go
+++ read-model-updater/cmd/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
-	gw "github.com/miyamo2/blogapi.miyamo.today/core/db/gorm"
 	"github.com/miyamo2/blogapi.miyamo.today/read-model-updater/internal/if-adapters/model"
-	nraws "github.com/newrelic/go-agent/v3/integrations/nrawssdk-v2"
-	"github.com/newrelic/go-agent/v3/integrations/nrlambda"
 	"github.com/newrelic/go-agent/v3/newrelic"
 	"gorm.io/gorm"
+	"log/slog"
 )
 
 // SyncHandler handles blogging event
@@ -38,8 +38,11 @@ func newDependencies(
 }
 
 func main() {
-	dep := getDependecies()
-	gw.Initialize(dep.gormDB)
-	nraws.AppendMiddlewares(&dep.awsConfig.APIOptions, nil)
-	nrlambda.Start(dep.synHandler, dep.newrelicApp)
+	lambda.Start(func(event events.DynamoDBEvent) {
+		slog.Info("receive", slog.Any("event", event))
+	})
+	//dep := getDependecies()
+	//gw.Initialize(dep.gormDB)
+	//nraws.AppendMiddlewares(&dep.awsConfig.APIOptions, nil)
+	//nrlambda.Start(dep.synHandler, dep.newrelicApp)
 }
