c0027a1ab40e44b78fd4e9a8b91affa1752e4a99
miyamo2 <miyamo2@outlook.com>
2024-12-14 05:42:17 +0900

    fix(read-model-updater): without ping to dynamodb

--- read-model-updater/internal/configs/di/provider.go
+++ read-model-updater/internal/configs/di/provider.go
@@ -83,10 +83,6 @@ func provideRDBGORM() *rdb.DB {
 
 func provideDynamoDBGORM(awsConfig *aws.Config) *dynamo.DB {
 	db := sql.OpenDB(pqxd.NewConnector(*awsConfig))
-	err := db.Ping()
-	if err != nil {
-		panic(err)
-	}
 	dynamoDialector := dynmgrm.New(dynmgrm.WithConnection(db))
 
 	// default connection
