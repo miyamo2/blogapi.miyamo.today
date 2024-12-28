ff950c72a81264200e66a56a3ca22472bdb48822
miyamo2 <miyamo2@outlook.com>
2024-12-29 06:23:19 +0900

    doc(core/echo): added middleware to instruments connect rpc

--- core/echo/middlewares/middleware.go
+++ core/echo/middlewares/middleware.go
@@ -7,6 +7,7 @@ import (
 	"github.com/miyamo2/blogapi.miyamo.today/core/log"
 	"github.com/newrelic/go-agent/v3/newrelic"
 	"github.com/oklog/ulid/v2"
+	"net/url"
 )
 
 func SetBlogAPIContextToContext(requestType blogapicontext.RequestType) echo.MiddlewareFunc {
@@ -22,7 +23,7 @@ func SetBlogAPIContextToContext(requestType blogapicontext.RequestType) echo.Mid
 				}
 				return ulid.Make().String()
 			}()
-			ctx = blogapicontext.StoreToContext(ctx, blogapicontext.New(rid, c.Path(), requestType, headers, nil))
+			ctx = blogapicontext.StoreToContext(ctx, blogapicontext.New(rid, c.Request().URL.Path, requestType, headers, nil))
 			c.SetRequest(c.Request().WithContext(ctx))
 			seg.End()
 			return next(c)
@@ -42,6 +43,32 @@ func SetLoggerToContext(app *newrelic.Application) echo.MiddlewareFunc {
 			}
 			c.SetRequest(c.Request().WithContext(ctx))
 			seg.End()
+			return next(c)
+		}
+	}
+}
+
+func NRConnect(app *newrelic.Application) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			ctx := c.Request().Context()
+			nrtx := newrelic.FromContext(ctx)
+			if nrtx == nil {
+				nrtx = app.StartTransaction(c.Request().URL.Path)
+				defer nrtx.End()
+			} else {
+				nrtx.SetName(c.Request().URL.Path)
+			}
+			host := c.Request().Host
+			nrtx.SetWebRequest(newrelic.WebRequest{
+				Type:      "ConnectRPC",
+				Host:      host,
+				Header:    c.Request().Header,
+				URL:       &url.URL{Scheme: "connectrpc", Host: host, Path: c.Request().URL.Path},
+				Method:    "POST",
+				Transport: newrelic.TransportHTTP})
+			ctx = newrelic.NewContext(ctx, nrtx)
+			c.SetRequest(c.Request().WithContext(ctx))
 			return next(c)
 		}
 	}
