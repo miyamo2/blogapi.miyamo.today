d29981bc02b011ce88a8043c0d9bd436ff596daf
miyamo2 <miyamo2@outlook.com>
2025-03-23 09:30:40 +0900

    fix(federator): arguments for `connect` client constructor

--- federator/internal/configs/di/provider/grpc_client.go
+++ federator/internal/configs/di/provider/grpc_client.go
@@ -5,6 +5,7 @@ import (
 	"blogapi.miyamo.today/federator/internal/infra/grpc/blogging_event/blogging_eventconnect"
 	"blogapi.miyamo.today/federator/internal/infra/grpc/tag/tagconnect"
 	"connectrpc.com/connect"
+	"fmt"
 	"github.com/google/wire"
 	"net/http"
 	"os"
@@ -12,17 +13,17 @@ import (
 
 func ArticleClient(httpClient *http.Client) articleconnect.ArticleServiceClient {
 	address := os.Getenv("ARTICLE_SERVICE_ADDRESS")
-	return articleconnect.NewArticleServiceClient(httpClient, address, connect.WithGRPC())
+	return articleconnect.NewArticleServiceClient(httpClient, fmt.Sprintf("http://%s", address), connect.WithGRPC())
 }
 
 func TagClient(httpClient *http.Client) tagconnect.TagServiceClient {
 	address := os.Getenv("TAG_SERVICE_ADDRESS")
-	return tagconnect.NewTagServiceClient(httpClient, address, connect.WithGRPC())
+	return tagconnect.NewTagServiceClient(httpClient, fmt.Sprintf("http://%s", address), connect.WithGRPC())
 }
 
 func BloggingEventClient(httpClient *http.Client) blogging_eventconnect.BloggingEventServiceClient {
 	address := os.Getenv("BLOGGING_EVENT_SERVICE_ADDRESS")
-	return blogging_eventconnect.NewBloggingEventServiceClient(httpClient, address, connect.WithGRPC())
+	return blogging_eventconnect.NewBloggingEventServiceClient(httpClient, fmt.Sprintf("http://%s", address), connect.WithGRPC())
 }
 
 var GRPCClientSet = wire.NewSet(ArticleClient, TagClient, BloggingEventClient)
