3c0bda410bd110798308975028e42599502eace2
miyamo2 <miyamo2@outlook.com>
2025-10-15 00:38:22 +0900

    fix(read-model-updater): logs in context

--- read-model-updater/cmd/main.go
+++ read-model-updater/cmd/main.go
@@ -5,11 +5,11 @@ import (
 	"log/slog"
 	"os"
 
-	"blogapi.miyamo.today/core/log"
 	"blogapi.miyamo.today/read-model-updater/internal/configs/di"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodbstreams"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodbstreams/types"
+	"github.com/miyamo2/altnrslog"
 	"github.com/newrelic/go-agent/v3/integrations/nrpkgerrors"
 	"github.com/newrelic/go-agent/v3/newrelic"
 	"golang.org/x/sync/errgroup"
@@ -17,10 +17,10 @@ import (
 
 func main() {
 	dependencies := di.GetDependecies()
-	slog.SetDefault(log.New())
 
 	tx := dependencies.NewRelicApp.StartTransaction(*dependencies.StreamARN)
 	ctx := newrelic.NewContext(context.Background(), tx)
+	logger := slog.New(altnrslog.NewTransactionalHandler(dependencies.NewRelicApp, tx))
 
 	describeStreamOutput, err := dependencies.StreamClient.DescribeStream(
 		ctx, &dynamodbstreams.DescribeStreamInput{
@@ -28,7 +28,7 @@ func main() {
 		},
 	)
 	if err != nil {
-		slog.Default().Error("failed to describe stream", slog.Any("error", err))
+		logger.Error("failed to describe stream", slog.Any("error", err))
 		tx.NoticeError(nrpkgerrors.Wrap(err))
 		os.Exit(1)
 	}
@@ -37,14 +37,14 @@ func main() {
 		select {
 		case <-egCtx.Done():
 			err := context.Cause(egCtx)
-			slog.Default().Error("error processing shards", slog.Any("error", err))
+			logger.Error("error processing shards", slog.Any("error", err))
 			tx.NoticeError(nrpkgerrors.Wrap(err))
 			os.Exit(1)
 		default:
 			eg.Go(
 				func() error {
 					shardID := *shard.ShardId
-					slog.Default().Info("processing shard", slog.String("shard_id", shardID))
+					logger.Info("processing shard", slog.String("shard_id", shardID))
 
 					getShardIteratorOutput, err := dependencies.StreamClient.GetShardIterator(
 						ctx, &dynamodbstreams.GetShardIteratorInput{
@@ -80,7 +80,7 @@ func main() {
 		}
 	}
 	if err := eg.Wait(); err != nil {
-		slog.Default().Error("error processing shards", slog.Any("error", err))
+		logger.Error("error processing shards", slog.Any("error", err))
 		tx.NoticeError(nrpkgerrors.Wrap(err))
 		os.Exit(1)
 	}
