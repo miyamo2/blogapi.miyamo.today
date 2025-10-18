db11a7682ad6fccc29048f91e15224cdff37f48e
miyamo2 <miyamo2@outlook.com>
2025-10-18 23:44:03 +0900

    feature(read-model-updater): shard processing to sequential

--- read-model-updater/cmd/main.go
+++ read-model-updater/cmd/main.go
@@ -14,7 +14,6 @@ import (
 	"github.com/miyamo2/altnrslog"
 	"github.com/newrelic/go-agent/v3/integrations/nrpkgerrors"
 	"github.com/newrelic/go-agent/v3/newrelic"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -52,60 +51,57 @@ func do(ctx context.Context, dependencies *di.Dependencies) error {
 	if err != nil {
 		return errors.Wrap(err, "failed to describe stream")
 	}
-	eg, egCtx := errgroup.WithContext(ctx)
 	for _, shard := range describeStreamOutput.StreamDescription.Shards {
-		select {
-		case <-egCtx.Done():
-			err := context.Cause(egCtx)
-			return errors.Wrap(err, "error processing shards")
-		default:
-			eg.Go(
-				func() error {
-					shardID := *shard.ShardId
-					slog.Default().InfoContext(egCtx, "processing shard", slog.String("shard_id", shardID))
-					defer slog.Default().InfoContext(egCtx, "processed shard", slog.String("shard_id", shardID))
+		err := func() error {
+			tx := newrelic.FromContext(ctx)
+			seg := tx.StartSegment("process shard")
 
-					blogAPICtx := blogapictx.New(shardID, *dependencies.StreamARN, "queue", nil, nil)
-					egCtx := blogapictx.StoreToContext(egCtx, blogAPICtx)
+			shardID := *shard.ShardId
+			seg.AddAttribute("shard_id", *shard.ShardId)
+			defer seg.End()
 
-					getShardIteratorOutput, err := dependencies.StreamClient.GetShardIterator(
-						egCtx, &dynamodbstreams.GetShardIteratorInput{
-							StreamArn:         dependencies.StreamARN,
-							ShardId:           &shardID,
-							ShardIteratorType: types.ShardIteratorTypeTrimHorizon,
-						},
-					)
-					if err != nil {
-						return err
-					}
-					shardIterator := getShardIteratorOutput.ShardIterator
+			slog.Default().InfoContext(ctx, "processing shard", slog.String("shard_id", shardID))
+			defer slog.Default().InfoContext(ctx, "processed shard", slog.String("shard_id", shardID))
 
-					for {
-						getRecordsOutput, err := dependencies.StreamClient.GetRecords(
-							egCtx, &dynamodbstreams.GetRecordsInput{
-								ShardIterator: shardIterator,
-								Limit:         aws.Int32(1000),
-							},
-						)
-						if err != nil {
-							return err
-						}
-						err = dependencies.SyncHandler.Invoke(egCtx, getRecordsOutput.Records)
-						if err != nil {
-							return err
-						}
-						shardIterator = getRecordsOutput.NextShardIterator
-						if shardIterator == nil {
-							break
-						}
-					}
-					return nil
+			blogAPICtx := blogapictx.New(shardID, *dependencies.StreamARN, "queue", nil, nil)
+			ctx := blogapictx.StoreToContext(ctx, blogAPICtx)
+
+			getShardIteratorOutput, err := dependencies.StreamClient.GetShardIterator(
+				ctx, &dynamodbstreams.GetShardIteratorInput{
+					StreamArn:         dependencies.StreamARN,
+					ShardId:           &shardID,
+					ShardIteratorType: types.ShardIteratorTypeTrimHorizon,
 				},
 			)
+			if err != nil {
+				return err
+			}
+			shardIterator := getShardIteratorOutput.ShardIterator
+
+			for {
+				getRecordsOutput, err := dependencies.StreamClient.GetRecords(
+					ctx, &dynamodbstreams.GetRecordsInput{
+						ShardIterator: shardIterator,
+						Limit:         aws.Int32(1000),
+					},
+				)
+				if err != nil {
+					return err
+				}
+				err = dependencies.SyncHandler.Invoke(ctx, getRecordsOutput.Records)
+				if err != nil {
+					return err
+				}
+				shardIterator = getRecordsOutput.NextShardIterator
+				if shardIterator == nil {
+					break
+				}
+			}
+			return nil
+		}()
+		if err != nil {
+			return errors.Wrap(err, "error processing shards")
 		}
-	}
-	if err := eg.Wait(); err != nil {
-		return errors.Wrap(err, "error processing shards")
 	}
 	return nil
 }
