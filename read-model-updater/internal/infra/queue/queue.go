cd240532f56f555cb68c41e2d68f16fd5e93b34b
miyamo2 <miyamo2@outlook.com>
2025-10-19 23:10:16 +0900

    feature(read-model-updater): migrate to use SQS for message handling

---
+++ read-model-updater/internal/infra/queue/queue.go
@@ -0,0 +1,16 @@
+package queue
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+)
+
+var _ Client = (*sqs.Client)(nil)
+
+type Client interface {
+	ReceiveMessage(
+		ctx context.Context, params *sqs.ReceiveMessageInput, optFns ...func(*sqs.Options),
+	) (*sqs.ReceiveMessageOutput, error)
+	DeleteMessage(ctx context.Context, params *sqs.DeleteMessageInput, optFns ...func(*sqs.Options)) (*sqs.DeleteMessageOutput, error)
+}
